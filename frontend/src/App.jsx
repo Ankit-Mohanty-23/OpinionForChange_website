6182f9ac4aa532d3af40fe43e6efa4f2d4dc6157
Ankit Mohanty <ankitmsb760@gmail.com>
2025-12-02 01:02:32 +0530

    Added new components and Protected the Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./components/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 import OpinaraLanding from "./pages/LandingPage";
-import OpinaraAuth from "./pages/LoginSignupPage"
-import TermsAndPolicy from "./assets/Terms&Policy";
+import OpinaraAuth from "./pages/LoginSignupPage";
+import TermsAndPolicy from "./components/Terms&Policy";
+import AuthSuccess from "./components/AuthSuccess";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <AuthProvider>
         <main>
-            <Routes>
-                <Route path="/" element={<OpinaraLanding />} />
-                <Route path="/login" element={<OpinaraAuth />} />
-                <Route path="/terms" element={<TermsAndPolicy />} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<OpinaraLanding />} />
+            <Route path="/login" element={<OpinaraAuth />} />
+            <Route path="/terms" element={<TermsAndPolicy />} />
+            <Route path="/auth/success" element={<AuthSuccess />} />
+            <Route 
+              path="/home" 
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              } />
+          </Routes>
         </main>
+      </AuthProvider>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
