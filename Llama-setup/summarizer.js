4d6427e60d5145bd51774f48363bef7b3b19ff1a
Ankit Mohanty <ankitmsb760@gmail.com>
2025-12-04 04:27:12 +0530

    Llama Summarizer for all posts

--- Llama-setup/summarizer.js
+++ Llama-setup/summarizer.js
@@ -2,19 +2,21 @@ import Groq from "groq-sdk";
 import dotenv from "dotenv";
 dotenv.config();
 
-const client = new Groq({ apikey: process.env.Groq_API_KEY });
+const client = new Groq({
+    apiKey: process.env.GROQ_API_KEY
+});
 
-export async function summarize(text){
+export async function summarize(text) {
     const response = await client.chat.completions.create({
         model: "llama-3.1-8b-instant",
-        message: [
+        messages: [
             {
                 role: "system",
                 content: "You summarize long social media posts into short, clear summaries."
             },
             {
                 role: "user",
-                message: `Summarize this: ${text}`
+                content: `Summarize this: ${text}`
             }
         ],
         temperature: 0.3
