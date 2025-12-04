bdaa9786a4f0bc75bf7c04ae9700ce4ac5d48c18
Ankit Mohanty <ankitmsb760@gmail.com>
2025-12-05 05:01:54 +0530

    Setup Profile_pic api

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import auth from "../middleware/auth.middleware.js";
+import upload from "../middleware/upload.middleware.js";
 import {
   login,
   signup,
   getUser,
   SendOtp,
   addBio,
+  addProfilePic,
 } from "../controller/user.controller.js";
 
 const Router = express.Router();
@@ -15,5 +17,6 @@ Router.post("/login", login);
 Router.post("/verify", SendOtp);
 Router.get("/data", auth, getUser);
 Router.put("/bio", auth, addBio);
+Router.post("/profile-pic", auth, upload.single("profile_pic"), addProfilePic);
 
 export default Router;
