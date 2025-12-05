c9fb94ec3bbeda976d01f0e06458bbcf86b93ce9
Ankit Mohanty <ankitmsb760@gmail.com>
2025-12-06 05:27:53 +0530

    Add Comment model with nested comment support

---
+++ backend/models/comment.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+    {
+        postId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+            required: true,
+            index: true,
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            index: true,
+        },
+        text: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 1000,
+        },
+        parentCommentId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment",
+            default: null,
+            index: true,
+        },
+    },{ timestamps: true }
+);
+
+commentSchema.index({ createdAt: -1 });
+export default mongoose.model("Comment", commentSchema);
