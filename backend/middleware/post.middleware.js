a4b741ea72fe293bd273209e55c44e7a6c5f2212
Ankit Mohanty <ankitmsb760@gmail.com>
2025-12-06 05:27:51 +0530

    Add post validation middleware for request validation

---
+++ backend/middleware/post.middleware.js
@@ -0,0 +1,43 @@
+export const validateCreatePost = (schema) => (req, res, next) => {
+    try{
+        if(schema.body){
+            const bodyResult = schema.body.safeParse(req.body);
+            if(!bodyResult.success){
+                return res.status(400).json({
+                    success: false,
+                    errors: bodyResult.error.issues.map((e) => e.message),
+                });
+            }
+            req.body = bodyResult.data;
+        }
+
+        if(schema.params){
+            const paramsResult = schema.body.safeParse(req.params);
+            if(!paramsResult.success){
+                return res.status(400).json({
+                    success: false,
+                    errors: paramsResult.error.issues.map((e) => e.message),
+                })
+            }
+            req.params = paramsResult.data;
+        }
+
+        if(schema.user){
+            const userResult = schema.user.safeParse(req.user);
+            if(!userResult.success){
+                return res.status(400).json({
+                    success: false,
+                    errors: userResult.error.issues.map((e) => e.message),
+                });
+            }
+            req.user = userResult.data;
+        }
+        next();
+    }catch(err){
+        console.log("Validation Error: ", err);
+        return res.status(500).json({
+            success: false,
+            msg: "Failed to Validate created Post"
+        });
+    }
+};
