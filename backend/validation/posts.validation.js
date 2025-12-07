4bb95fdd451ef88c70b8910ec05cc6fac738ea18
Ankit Mohanty <ankitmsb760@gmail.com>
2025-12-08 04:13:08 +0530

    refactor: update post validation schemas with ObjectId validation

--- backend/validation/posts.validation.js
+++ backend/validation/posts.validation.js
@@ -4,22 +4,8 @@ import mongoose from "mongoose";
 const objectSchema = z.string().refine(
     (id) => mongoose.Types.ObjectId.isValid(id), {
         message: "Invalid ObjectId format",
-      });
-
-export const createPostSchema = {
-    body: z.object({
-        title: z.string().trim().min(1, "Title is missing"),
-        content: z.string().trim().min(1, "Content is missing"),
-    }),
-
-    params: getWaveId.waveId,
-    user: getUserId._id,
-};
-
-export const deletePostSchema = {
-    user: getUserId._id,
-    params: getPostId.params
-}
+    }
+);
 
 export const getUserId = {
     user: z.object({
@@ -38,3 +24,19 @@ export const getWaveId = {
         waveId: objectSchema,
     })
 }
+
+export const createPostSchema = {
+    body: z.object({
+        title: z.string().trim().min(1, "Title is missing"),
+        content: z.string().trim().min(1, "Content is missing"),
+    }),
+
+    params: getWaveId.waveId,
+    user: getUserId._id,
+};
+
+export const deletePostSchema = {
+    user: getUserId._id,
+    params: getPostId.params
+}
+
