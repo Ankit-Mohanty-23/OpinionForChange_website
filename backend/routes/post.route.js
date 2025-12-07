74594841daf08514ec58b78fa37b5653e07a81fc
Ankit Mohanty <ankitmsb760@gmail.com>
2025-12-07 05:37:14 +0530

    Update post routes

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,15 +1,36 @@
 import express from "express";
 import upload from "../middleware/upload.middleware.js";
 import auth from "../middleware/auth.middleware.js";
-import { createPost ,getAllPosts, getPost, deletePost, toggleVote, addComment} from "../controller/post.controller.js";
+import {
+  createPost,
+  getAllPosts,
+  getPost,
+  deletePost,
+  toggleVote,
+  addComment,
+} from "../controller/post.controller.js";
+import {
+  getUserId,
+  getPostId,
+  getWaveId,
+  createPostSchema,
+  deletePostSchema,
+} from "../validation/posts.validation.js";
+import validatePost from "../middleware/post.middleware.js";
 
 const Router = express.Router();
 
-Router.post("/create", auth, upload.array("media", 5), createPost);
-Router.get("/all/:userId", getAllPosts);
-Router.get("/one/:postId", getPost);
-Router.delete("/:postId", auth, deletePost);
+Router.post(
+  "/create",
+  auth,
+  validatePost(createPostSchema),
+  upload.array("media", 5),
+  createPost
+);
+Router.get("/posts", auth, validatePost(createPostSchema), getAllPosts);
+Router.get("/:postId", auth, validatePost(getPostId), getPost);
+Router.delete("/:postId", auth, validatePost(deletePostSchema), deletePost);
 Router.patch("/:postId/vote", auth, toggleVote);
 Router.patch("/:postId/comment", auth, addComment);
 
-export default Router;  
+export default Router;
