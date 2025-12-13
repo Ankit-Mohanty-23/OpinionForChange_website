fbc1a36c1a22ccd89ee6c12dd604d0348b344c07
Ankit Mohanty <ankitmsb760@gmail.com>
2025-12-13 23:38:06 +0530

    Update wave controller

--- backend/controller/wave.controller.js
+++ backend/controller/wave.controller.js
@@ -1,6 +1,6 @@
 import Wave from "../models/wave.model.js";
 import { V2 as cloudinary } from "cloudinary";
-import Post from "../models/post.model.js"
+import Post from "../models/post.model.js";
 import summarize from "/Llama-setup/summarizer.js";
 
 /**
@@ -66,9 +66,9 @@ export async function getWavePosts(req, res) {
   try {
     const userId = req.user?._id;
     const waveId = req.params;
-    
+
     const page = Number(req.query.page) || 1;
-    const skip = (page-1) * 10;
+    const skip = (page - 1) * 10;
 
     const posts = await Post.find({ waveId })
       .sort({ createdAt: -1 })
@@ -77,13 +77,13 @@ export async function getWavePosts(req, res) {
       .select("title content media upvotes downvotes createdAt") // field selection
       .populate("userId", "fullname profilePic");
 
-    if(posts.length === 0){
+    if (posts.length === 0) {
       return res.status(400).json({
         success: false,
-        msg: "Posts not found!"
-      })
+        msg: "Posts not found!",
+      });
     }
-    
+
     res.status(200).json({
       success: true,
       page,
